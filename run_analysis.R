9109851ef34746357b3d86db1afc1bdf0a985686
Osama Al-Eryani <oeryania@gmail.com>
2023-09-22 17:21:32 -0400

    cleanup & comments

--- run_analysis.R
+++ run_analysis.R
@@ -9,19 +9,40 @@ library(dplyr)
        features <- read.table("./data/features.txt")
 activity_labels <- read.table("./data/activity_labels.txt")
 
-x <- rbind(x_train, x_test)
-names(x) <- features$V2
+# X columns | merge train & test rows + use descriptive feature names + extract mean & std
+measurements <- rbind(x_train, x_test)
+names(measurements) <- features$V2
+measurements <- measurements %>% select(matches("-mean\\(\\)|-std\\(\\)"))
 
-x <- select(x, matches("-mean\\(\\)|-std\\(\\)"))
+# Y column | merge train & test rows + use descriptive activity labels
+activity <- rbind(y_train, y_test)
+activity_labels <- split(activity_labels$V2, activity_labels$V1)
+activity <- activity %>% unlist %>% lapply(function(e){activity_labels[[e]]}) %>% unlist(use.names = FALSE)
 
-y <- rbind(y_train, y_test)
-y <- lapply(y, function(x){activity_labels$V2[x]}) %>% unlist
+# SUBJECT column | merge train & test rows
+subject <- rbind(subject_train, subject_test) %>% unlist(use.names = FALSE)
 
-yx <- cbind(activity = y, x)
-rownames(yx) <- NULL
+# FULL DATA | merge all columns
+data_full <- cbind(subject, activity, measurements)
 
-s <- rbind(subject_train, subject_test)
-syx <- cbind(s, yx)
+# TIDY DATA | summarize data by activity's average measurements for each subject
+data_tidy <- data_full %>% group_by(subject, activity) %>% summarize_all(mean)
 
-syx <- rename(syx, subject = V1)
-syx_avg <- syx %>% group_by(subject, activity) %>% summarize_all(mean)
+# clean variable names
+names(data_tidy) <- names(data_full) %>%
+    gsub(x=., "(-)std\\(\\)", "\\1standard-deviation\\2") %>%
+    gsub(x=., "\\(|\\)", "") %>%
+    gsub(x=., "^t([A-Z])", "\\1") %>%
+    gsub(x=., "^f([A-Z])", "fft-\\1") %>%
+    gsub(x=., "([-][X|Y|Z])", "\\1-axis") %>%
+    gsub(x=., "Acc", "Acceleration") %>%
+    gsub(x=., "Gyro", "AngularVelocity") %>%
+    gsub(x=., "([a-z])([A-Z])", "\\1_\\2") %>%
+    gsub(x=., "-", "_") %>%
+    tolower
+
+
+# save tidy data to file
+write.table(data_tidy, "./data/data_tidy.txt", row.names = FALSE)
+
+write.csv(names(data_tidy), "./data/data_tidy_variables.csv", row.names = FALSE, quote = FALSE)
